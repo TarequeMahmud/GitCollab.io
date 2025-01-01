b9acc94fadbfc33b0e11027b01c8bde0180dd3aa
TarequeMahmud <107052189+TarequeMahmud@users.noreply.github.com>
2025-01-01 22:02:11 +0600

    feat(server): set up the schema for project.

---
+++ Server/src/models/Project.js
@@ -0,0 +1,20 @@
+const projectSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  deadline: { type: Date, required: true },
+  people: [
+    {
+      user_id: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+      role: {
+        type: String,
+        enum: ["admin", "manager", "member"],
+      },
+    },
+  ],
+  tasks: [{ type: mongoose.Schema.Types.ObjectId, ref: "Task" }],
+  created_by: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+});
--- Server/src/models/User.js
+++ Server/src/models/User.js
@@ -4,11 +4,15 @@ const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  role: {
-    type: String,
-    enum: ["admin", "manager", "member"],
-    default: "member",
-  },
+  projects: [
+    {
+      project_id: { type: mongoose.Schema.Types.ObjectId },
+      role: {
+        type: String,
+        enum: ["admin", "manager", "member"],
+      },
+    },
+  ],
 });
 
 export default mongoose.model("User", userSchema);
