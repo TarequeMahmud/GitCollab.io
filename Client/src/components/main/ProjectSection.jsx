30d1949494d39b379e73b309bbf462698061be93
TarequeMahmud <107052189+TarequeMahmud@users.noreply.github.com>
2025-01-13 11:49:06 +0600

    feat: add cards to show projects

--- Client/src/components/main/ProjectSection.jsx
+++ Client/src/components/main/ProjectSection.jsx
@@ -1,5 +1,26 @@
+import styles from "@styles/main/ProjectSection.module.scss";
+import datas from "@datas/bulkProjects.json";
 const ProjectSection = () => {
-  return <div>ProjectSection</div>;
+  return (
+    <>
+      <h1>My Projects</h1>
+      <div className={styles.container}>
+        {datas.map((data, index) => (
+          <div key={index} className={styles["project-card"]}>
+            <h4>{data.name}</h4>
+
+            <p className={styles.description}>
+              {data.description.length > 50
+                ? `${data.description.slice(0, 50)}...`
+                : data.description}
+            </p>
+            <hr />
+            <div className={styles["features-container"]}></div>
+          </div>
+        ))}
+      </div>
+    </>
+  );
 };
 
 export default ProjectSection;
