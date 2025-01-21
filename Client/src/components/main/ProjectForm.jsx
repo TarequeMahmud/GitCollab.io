162b87dd3132303a26b86d29bbfa7fe13ca72f61
TarequeMahmud <107052189+TarequeMahmud@users.noreply.github.com>
2025-01-21 19:01:26 +0600

    feat(styles, features, fixes in client): add multiple styles. refractor and improvement of the UI for the project submission form.

--- Client/src/components/main/ProjectForm.jsx
+++ Client/src/components/main/ProjectForm.jsx
@@ -1,33 +1,95 @@
 import styles from "@styles/main/ProjectForm.module.scss";
 const ProjectForm = () => {
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
   return (
     <div className={styles.container}>
       <h1>Create A New Project</h1>
-      <div className="form-container">
-        <p>Fill up the necessary fields</p>
+      <div className={styles["form-container"]}>
         <form action="" method="post">
-          <label htmlFor="title">Project Title</label>
-          <input type="text" name="title" />
-
-          <div className="project-owner">
-            <h3>Project Owner</h3>
-            <label htmlFor="full-name">Full Name</label>
-            <input type="text" name="full-name" />
-            <label htmlFor="username">User Name</label>
-            <input type="text" />
+          {/* title input section */}
+          <div className={styles["input-container"]}>
+            <label htmlFor="title" className={styles["input-title"]}>
+              Project Title:
+            </label>
+            <input type="text" name="title" required />
           </div>
 
-          <label htmlFor="deadline">Deadline</label>
-          <input type="date" name="" id="" />
-          <div className="project-description">
-            <label htmlFor="description"></label>
+          {/* name input section */}
+
+          <div className={`${styles["input-container"]} `}>
+            <p className={styles["input-title"]}>Project Owner:</p>
+            <div className={`${styles["name-container"]} `}>
+              <div className={styles["label-input-holder"]}>
+                <label htmlFor="full-name">Full Name:</label>
+                <input type="text" name="full-name" required />
+              </div>
+
+              <div className={styles["label-input-holder"]}>
+                <label htmlFor="username">Username:</label>
+                <input type="text" required />
+              </div>
+            </div>
+            {/* deadline */}
+            <div className={`${styles["input-container"]} ${styles.deadline}`}>
+              <p className={styles["input-title"]}>Deadline:</p>
+              <div className={`${styles["name-container"]} `}>
+                <select name="day" id="day">
+                  <option value="0">Day</option>
+                  {[...Array(31).keys()].map((_, index) => (
+                    <option key={index + 1} value={index + 1}>
+                      {index + 1}
+                    </option>
+                  ))}
+                </select>
+                <select name="month" id="month">
+                  <option value="0">Month</option>
+                  {months.map((month, index) => (
+                    <option key={index} value={index}>
+                      {month}
+                    </option>
+                  ))}
+                </select>
+                <select name="years" id="years">
+                  <option value="0">Year</option>
+                  {[...Array(10).keys()].map((_, index) => (
+                    <option key={index + 2025} value={index + 2025}>
+                      {index + 2025}
+                    </option>
+                  ))}
+                </select>
+              </div>
+            </div>
+          </div>
+          {/* description section */}
+
+          <div
+            className={`${styles["input-container"]} ${styles["project-description"]}`}
+          >
+            <label htmlFor="description" className={styles["input-title"]}>
+              Description:
+            </label>
             <textarea
               name="description"
               id="description"
-              cols="30"
+              cols="25"
               rows="10"
             ></textarea>
           </div>
+          <button type="submit">CREATE</button>
         </form>
       </div>
     </div>
