dc5616231dd3a8535f1a74d6d11de69e3c680177
TarequeMahmud <107052189+TarequeMahmud@users.noreply.github.com>
2025-01-23 04:30:02 +0600

    feat(client): add features in project card

---
+++ Client/src/assets/datas/cardFeatures.js
@@ -0,0 +1,20 @@
+import admin from "@icons/card-features/admin.png";
+import deadline from "@icons/card-features/deadline.png";
+import management from "@icons/card-features/management.png";
+import percent from "@icons/card-features/percent.png";
+import total from "@icons/card-features/total.png";
+import user from "@icons/card-features/user.png";
+import start from "@icons/card-features/start.png";
+import datas from "@datas/bulkProjects.json";
+
+const cardIcons = {
+  admin,
+  deadline,
+  management,
+  percent,
+  total,
+  user,
+  start,
+};
+
+export default cardIcons;
--- Client/src/components/main/ProjectSection.jsx
+++ Client/src/components/main/ProjectSection.jsx
@@ -2,24 +2,34 @@ import styles from "@styles/main/ProjectSection.module.scss";
 import { MdOutlineCreate } from "react-icons/md";
 import datas from "@datas/bulkProjects.json";
 import { useNavigate } from "react-router";
+import generateFeatures from "@utils/generateFeatures.js";
+import CardFeatures from "./CardFeatures";
 
 const ProjectSection = () => {
   const navigate = useNavigate();
   return (
     <>
-      <h1>My Projects</h1>
+      <h1 className={styles.heading}>MY PROJECTS</h1>
       <div className={styles.container}>
         {datas.map((data, index) => (
           <div key={index} className={styles["project-card"]}>
-            <h4>{data.name}</h4>
+            <h2>{data.name}</h2>
 
             <p className={styles.description}>
-              {data.description.length > 50
-                ? `${data.description.slice(0, 50)}...`
+              {data.description.length > 100
+                ? `${data.description.slice(0, 100)}...`
                 : data.description}
             </p>
             <hr className={styles["card-separator"]} />
-            <div className={styles["features-container"]}>\</div>
+            <div className={styles["features-container"]}>
+              {generateFeatures(data).map((feature, index) => (
+                <CardFeatures
+                  key={index}
+                  icon={feature.icon}
+                  info={feature.info}
+                />
+              ))}
+            </div>
           </div>
         ))}
       </div>
