fce6f64f2bc6c4f7a1ab06375eb69775900a40fc
TarequeMahmud <107052189+TarequeMahmud@users.noreply.github.com>
2025-01-25 22:16:45 +0600

    feat(Client): add dropdown selector for logout and settings on account icon,

--- Client/src/components/hero/Account.jsx
+++ Client/src/components/hero/Account.jsx
@@ -1,10 +1,50 @@
+import { useState, useRef, useEffect } from "react";
 import icon from "../../assets/images/hero/user-regular.svg";
 import styles from "../../styles/hero/Account.module.scss";
+import useHideOutsideClick from "../../hooks/useHideOutsideClick";
+import { useNavigate } from "react-router";
+
 const Account = () => {
+  const navigate = useNavigate();
+
+  //Show or hide options
+  const [showOptions, setShowOptions] = useState(false);
+  const [hasUserData, setHasUserData] = useState(false);
+  useEffect(() => {
+    setHasUserData(!!localStorage.getItem("userdata"));
+  });
+  const ref = useRef();
+  useHideOutsideClick(ref, () => setShowOptions(false));
+  //Add logout functionalities,
+  const handleLogout = () => {
+    setShowOptions(false);
+    localStorage.removeItem("userdata");
+    navigate("/");
+  };
   return (
-    <div className={styles.container}>
-      <img src={icon} alt="account-logo" className={styles.img} />
-    </div>
+    <>
+      <div
+        className={styles.container}
+        ref={ref}
+        onClick={(e) => {
+          setShowOptions(!showOptions);
+        }}
+      >
+        <img src={icon} alt="account-logo" className={styles.img} />
+        {hasUserData && showOptions && (
+          <div
+            onClick={(e) => e.stopPropagation()}
+            className={styles["option-container"]}
+          >
+            <p onClick={handleLogout} className={styles.option}>
+              Logout
+            </p>
+            <hr />
+            <p className={styles.option}>Settings</p>
+          </div>
+        )}
+      </div>
+    </>
   );
 };
 
