c215a7d3cda12a3480b376cfedd52b0eb4de6e9a
TarequeMahmud <107052189+TarequeMahmud@users.noreply.github.com>
2025-01-30 22:54:33 +0600

    feat(server): 1. add functionality to see all projects. 2. add functionality to see specific project. 3. add security measures to prevent data leakage from server.

--- Server/src/routes/projectRoute.js
+++ Server/src/routes/projectRoute.js
@@ -7,13 +7,10 @@ const router = express.Router({ mergeParams: true });
 
 router.use(isAuthenticated);
 
-//TODO: complete the logic to fetch project data.
-
 router.get("/", async (req, res, next) => {
   const userId = req.params.userId;
   try {
     const response = await User.findById(userId).select("projects");
-    console.log(response.projects);
 
     if (!response) {
       return res.status(401).json({
@@ -21,36 +18,57 @@ router.get("/", async (req, res, next) => {
         message: "You must have an account to create a project!",
       });
     }
-
-    if (!response.projects || response.projects.length === 0) {
+    const projects = response.projects;
+    if (!projects || projects.length === 0) {
       return res.status(404).json({
         loggedIn: true,
         message: "No projects created yet.",
       });
     }
+    const projectsArray = await Promise.all(
+      projects.map((project) => Project.findById(project.project_id))
+    );
 
-    return res.json(response.projects);
+    return res.json(projectsArray);
   } catch (error) {
-    console.log(error);
-    res.status(500).json({ loggedIn: false, message: "Server error", error });
+    next(error);
   }
 });
 
-router.post("/:id", async (req, res, next) => {
+router.get("/:projectId", async (req, res, next) => {
+  const { userId, projectId } = req.params;
+
+  try {
+    const project = await Project.findById(projectId);
+    const isEnrolledUser = project.people.some(
+      (person) => person.user_id.toString() === userId
+    );
+    if (isEnrolledUser) {
+      return res.status(200).json(project);
+    } else {
+      return res.status(404).json({
+        message: "No project found",
+      });
+    }
+  } catch (error) {
+    return next(error);
+  }
+});
+
+router.post("/", async (req, res, next) => {
   //Fetch the id of creator from parameters
-  const createdBy = req.params.id;
+  const creator = req.params.userId;
 
   //verify the creator, if it is valid account.
-  const verifyCreator = await User.findById(createdBy);
-  if (!verifyCreator) {
+  const creatorInfo = await User.findById(creator);
+  if (!creatorInfo) {
     return res.status(401).json({
       loggedIn: false,
       message: "You must have an account to create a project!",
     });
   }
 
-  //
-  const { name, description, deadline, people, tasks } = req.body;
+  const { name, description, deadline } = req.body;
   if (!name) return res.status(400).json({ message: "Insert a Name" });
 
   try {
@@ -58,16 +76,20 @@ router.post("/:id", async (req, res, next) => {
       name,
       description,
       deadline,
-      people,
-      tasks,
-      created_by: createdBy,
+      people: [{ user_id: creatorInfo._id, role: "admin" }],
+      created_by: creatorInfo._id,
     });
     const saveProject = await newProject.save();
+    creatorInfo.projects.push({ project_id: saveProject._id, role: "admin" });
+    const updateCreatorInfo = await creatorInfo.save();
 
-    res.status(201).json({ message: "Project created successfully" });
+    return res.status(201).json({
+      message: "Project created successfully",
+      project: saveProject.people,
+      projectsList: updateCreatorInfo.projects,
+    });
   } catch (error) {
-    console.error(error);
-    res.status(500).json({ loggedIn: false, message: "Server error", error });
+    next(error);
   }
 });
 
