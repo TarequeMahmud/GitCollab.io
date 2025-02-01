64bd8353235c87cf3f663ccaf54ab12f3ba8283c
TarequeMahmud <107052189+TarequeMahmud@users.noreply.github.com>
2025-02-01 10:08:08 +0600

    feat(client): 1. add alert bar to show alert. 2. style the alertbar. 3. add the functionality to hide the alertbar on clicking outside.

--- Client/src/components/hero/AlertBar.jsx
+++ Client/src/components/hero/AlertBar.jsx
@@ -1,10 +1,12 @@
 import styles from "@styles/hero/AlertBar.module.scss";
-const AlertBar = ({ title, data }) => {
+const AlertBar = ({ title, data, onClick }) => {
   return (
     <div className={styles.container}>
       <h3>{title}</h3>
       <hr />
       <p>{data}</p>
+      <hr />
+      <button onClick={onClick}>Ok</button>
     </div>
   );
 };
--- Client/src/components/hero/Nav.jsx
+++ Client/src/components/hero/Nav.jsx
@@ -1,16 +1,48 @@
-import style from "../../styles/hero/Nav.module.scss";
+import styles from "../../styles/hero/Nav.module.scss";
 import { navbarItems } from "@datas/navbar.json";
-import { NavLink } from "react-router-dom";
+
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router";
+import AlertBar from "./AlertBar";
 const Nav = () => {
+  const navigate = useNavigate();
+  //if has user data
+  const [hasUserData, setHasUserData] = useState(false);
+  const [showAlert, setShowAlert] = useState(null);
+  useEffect(() => {
+    setHasUserData(!!localStorage.getItem("userdata"));
+  });
+
+  const handleClick = (link, data) => {
+    if (hasUserData) {
+      navigate(link);
+    } else {
+      setShowAlert(data);
+      setTimeout(() => {
+        setShowAlert(null);
+      }, 3000);
+    }
+  };
+  const handleButtonClick = () => {
+    setShowAlert(null);
+  };
+
   return (
-    <div className={style.container}>
+    <div className={styles.container}>
       <ul>
         {navbarItems.map((items, index) => (
-          <NavLink key={index} to={items.link}>
-            <li>{items.name}</li>
-          </NavLink>
+          <li onClick={() => handleClick(items.link, items.name)} key={index}>
+            {items.name}
+          </li>
         ))}
       </ul>
+      {showAlert && (
+        <AlertBar
+          title={"Login Required"}
+          data={`To continue with ${showAlert} you must be a valid user. Please login or Register to create an account.`}
+          onClick={handleButtonClick}
+        />
+      )}
     </div>
   );
 };
