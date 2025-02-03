9bcc657df318f4881aaf81f539b171c48e0138a5
TarequeMahmud <107052189+TarequeMahmud@users.noreply.github.com>
2025-02-03 10:49:38 +0600

    feat(server): add functionality to add users to a project admin

--- Server/src/routes/projectRoute.js
+++ Server/src/routes/projectRoute.js
@@ -7,6 +7,8 @@ const router = express.Router({ mergeParams: true });
 
 router.use(isAuthenticated);
 
+/*This Section is for admin*/
+
 router.get("/", async (req, res, next) => {
   const userId = req.params.userId;
   try {
@@ -76,7 +78,9 @@ router.post("/", async (req, res, next) => {
       name,
       description,
       deadline,
-      people: [{ user_id: creatorInfo._id, role: "admin" }],
+      people: [
+        { user_id: creatorInfo._id, name: creatorInfo.name, role: "admin" },
+      ],
       created_by: creatorInfo._id,
     });
     const saveProject = await newProject.save();
@@ -88,6 +92,65 @@ router.post("/", async (req, res, next) => {
       project: saveProject.people,
       projectsList: updateCreatorInfo.projects,
     });
+  } catch (error) {
+    next(error);
+  }
+});
+
+//user routes for admin
+router.post("/:projectId/users", async (req, res, next) => {
+  const creator = req.user._id;
+  const { username, role } = req.body;
+  const projectId = req.params.projectId;
+
+  try {
+    if (!username || !role) {
+      return res.status(400).json({
+        message: "Username and role are required.",
+      });
+    }
+    const user = await User.findOne({ username: username });
+
+    if (!user) {
+      return res.status(404).json({
+        message: "User not found",
+      });
+    }
+    console.log(user);
+
+    const project = await Project.findById(projectId);
+    if (!project) {
+      return res.status(404).json({
+        message: "Project not found",
+      });
+    }
+    //if user exists in the people list:
+    const isExist = project.people.some(
+      (person) => person?.user_id?.toString() === user._id?.toString()
+    );
+    if (isExist)
+      return res
+        .status(409)
+        .json({ message: "User already exists in this project." });
+    //verify if admin
+    const isAdmin = project.people.some(
+      (person) =>
+        person.user_id.toString() === creator.toString() &&
+        person.role === "admin"
+    );
+    if (!isAdmin) {
+      return res
+        .status(403)
+        .json({ message: "Unauthorized: Only admin can assign a task" });
+    }
+
+    //update project
+    project.people.push({ user_id: user._id, name: user.name, role: role });
+    const updateProjectInfo = await project.save();
+    if (!updateProjectInfo) {
+      return res.json("not updated");
+    }
+    res.status(201).json({ people: updateProjectInfo.people });
   } catch (error) {
     next(error);
   }
