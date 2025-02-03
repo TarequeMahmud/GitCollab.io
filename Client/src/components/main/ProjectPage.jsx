8e2c98f582cce09e2ed7421e201064421507951f
TarequeMahmud <107052189+TarequeMahmud@users.noreply.github.com>
2025-02-03 17:40:37 +0600

    feat(Client): 1. setup functionality to add user in project with specific role. 2. update the user table with every user added.

--- Client/src/components/main/ProjectPage.jsx
+++ Client/src/components/main/ProjectPage.jsx
@@ -15,17 +15,20 @@ const ProjectPage = () => {
   const [project, setProject] = useState({});
   const [people, setPeople] = useState([]);
   const [tasks, setTasks] = useState([]);
+  const [showUserForm, setShowUserForm] = useState(false);
+  const [username, setUsername] = useState("");
+  const [role, setRole] = useState("manager");
   //project id
   const { projectId } = useParams();
 
-  //retrieve the user id that passed from the project section
+  //--retrieve the user id that passed from the project section--
   const userId =
     location.state?.userId ||
     (storedUserId
       ? JSON.parse(sessionStorage.getItem(`project-${projectId}`)).userId
       : null);
 
-  //make api request to retrieve project data from database.
+  //--make api request to retrieve project data from database.--
   useEffect(() => {
     if (!projectId && !userId) return;
     const fetchProject = async () => {
@@ -62,6 +65,38 @@ const ProjectPage = () => {
     fetchProject();
   }, [projectId, userId]);
 
+  //--handle-click functions Here--
+  const handleAddUser = async () => {
+    if (!username) {
+      console.log("please enter the name");
+      return;
+    }
+    try {
+      const response = await fetch(
+        `http://localhost:5000/${userId}/project/${projectId}/users`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ username: username, role: role }),
+          credentials: "include",
+        }
+      );
+      const userData = await response.json();
+      setPeople(userData.people);
+    } catch (error) {
+      console.log(error);
+    }
+    console.log(username, role);
+    setShowUserForm(false);
+    setUsername("");
+    setRole("manager");
+  };
+
+  //hand
+
+  //conditional rendering
   if (loading) return <Spinner />;
   if (!project) return <h1>Project not found</h1>;
 
@@ -88,24 +123,47 @@ const ProjectPage = () => {
               table: styles.table,
             }}
           />
-          <button
-            className={`${styles.button} ${styles["button-add"]}`}
-            type="button"
-          >
-            <img src={addIcon} alt="add user icon" height={20} width={20} />
-            <p>Add Users</p>
-          </button>
+          <div className={styles["input-section"]}>
+            {!showUserForm ? (
+              <button
+                className={`${styles.button} ${styles["button-add"]}`}
+                onClick={() => setShowUserForm(true)}
+                type="button"
+              >
+                <img src={addIcon} alt="add user icon" height={20} width={20} />
+                <p>Add Users</p>
+              </button>
+            ) : (
+              <div className={styles["input-container"]}>
+                <input
+                  placeholder="Enter Username"
+                  name="add-user"
+                  type="text"
+                  onChange={(e) => setUsername(e.target.value)}
+                />
+                <select
+                  name="role"
+                  id="role"
+                  onChange={(e) => setRole(e.target.value)}
+                >
+                  <option value="Manager">Manager</option>
+                  <option value="member">Member</option>
+                </select>
+                <button
+                  type="button"
+                  name="submit"
+                  className={`${styles.button} ${styles["button-submit"]}`}
+                  onClick={handleAddUser}
+                >
+                  <p>Submit</p>
+                </button>
+              </div>
+            )}
+          </div>
         </div>
         <div className={styles["user-table-container"]}>
           <h3>Total tasks assigned</h3>
           <TaskList tasks={tasks} />
-          <button
-            className={`${styles.button} ${styles["button-add"]}`}
-            type="button"
-          >
-            <img src={addIcon} alt="add user icon" height={20} width={20} />
-            <p>Add Users</p>
-          </button>
         </div>
       </>
     </div>
