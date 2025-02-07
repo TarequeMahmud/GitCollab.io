42d272020c0ae15e415352d7c30189f134969c9a
TarequeMahmud <107052189+TarequeMahmud@users.noreply.github.com>
2025-02-07 11:31:32 +0600

    fix: fix the problem with alertbar timeout on clicking many elements that trigger alertbar. using useRef.

--- Client/src/contexts/AlertContext.jsx
+++ Client/src/contexts/AlertContext.jsx
@@ -1,16 +1,20 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useState, useRef } from "react";
 
 export const AlertContext = createContext();
 export const useAlert = () => useContext(AlertContext);
 
 export const AlertProvider = ({ children }) => {
   const [alert, setAlert] = useState(null);
-
+  const timeoutRef = useRef(null);
   const showAlert = (title, message) => {
+    if (timeoutRef.current) {
+      clearTimeout(timeoutRef.current);
+    }
     setAlert({ title, message });
 
-    setTimeout(() => {
+    timeoutRef.current = setTimeout(() => {
       setAlert(null);
+      timeoutRef.current = null;
     }, 3000);
   };
 
