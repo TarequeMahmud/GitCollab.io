adc658a9229183f0c1a54befe703af8c094bedd6
TarequeMahmud <107052189+TarequeMahmud@users.noreply.github.com>
2025-09-13 12:07:16 +0600

    feat(GCI-21): create tasks app in django and migrate the basic url endpoints to it

--- server/gitcollab/urls.py
+++ server/gitcollab/urls.py
@@ -6,11 +6,13 @@ from rest_framework.routers import DefaultRouter
 # import the routers from apps
 from users.urls import router as users_router
 from projects.urls import router as projects_router
+from tasks.urls import router as tasks_router
 
 # merge routers into one
 router = DefaultRouter()
 router.registry.extend(users_router.registry)
 router.registry.extend(projects_router.registry)
+router.registry.extend(tasks_router.registry)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
---
+++ server/tasks/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+
+class Task(models.Model):
+    STATUS_CHOICES = [
+        ("to-do", "To Do"),
+        ("in-progress", "In Progress"),
+        ("completed", "Completed"),
+    ]
+
+    PRIORITY_CHOICES = [
+        ("high", "High"),
+        ("medium", "Medium"),
+        ("low", "Low"),
+    ]
+
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    deadline = models.DateTimeField()
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES)
+    priority = models.CharField(max_length=20, choices=PRIORITY_CHOICES)
