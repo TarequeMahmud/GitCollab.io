a867e071a3a9aaf4454492c9eafbb9b6a1cc0e33
TarequeMahmud <107052189+TarequeMahmud@users.noreply.github.com>
2025-09-14 22:53:11 +0600

    feat(GCI-24): add permissions for operations and admin management on user account

--- server/accounts/admin.py
+++ server/accounts/admin.py
@@ -1,3 +1,43 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User
 
-# Register your models here.
+
+class UserAdmin(BaseUserAdmin):
+    ordering = ["email"]
+    list_display = ["email", "name", "username", "is_staff", "is_active"]
+    fieldsets = (
+        ("Authentication", {"fields": ("username", "password")}),
+        ("Personal Info", {"fields": ("name", "email", "about")}),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                )
+            },
+        ),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "name",
+                    "username",
+                    "email",
+                    "password1",
+                    "password2",
+                    "about",
+                    "is_active",
+                    "is_staff",
+                ),
+            },
+        ),
+    )
+
+
+admin.site.register(User, UserAdmin)
--- server/accounts/views.py
+++ server/accounts/views.py
@@ -1,9 +1,25 @@
-from re import L
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
+from rest_framework.permissions import AllowAny, IsAuthenticated, IsAdminUser
 from .models import User
 from .serializers import UserSerializer
+
+
+class IsSelfOrAdmin(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return request.user.is_staff or obj == request.user
 
 
 class UsersView(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+    def get_permissions(self):
+        if self.action == "create":
+            permission_classes = [AllowAny]
+        elif self.action == "list":
+            permission_classes = [IsAdminUser]
+        elif self.action in ["retrieve", "update", "partial_update", "destroy"]:
+            permission_classes = [IsSelfOrAdmin]
+        else:
+            permission_classes = [IsAuthenticated]
+        return [permission() for permission in permission_classes]
